368e5e693add229f26deae1e7e68cc2becc16d3a
akhilraj95 <akhil95raj@gmail.com>
2020-03-23 19:22:51 -0400

    Initial set up of js client

---
+++ dsynk-cl/js/webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/relay.js',
+  output: {
+    filename: 'dsynk.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'dsynk'    
+  }
+}
