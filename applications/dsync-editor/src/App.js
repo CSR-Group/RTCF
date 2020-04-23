af30a8504c468e0b0c8984a5007f360155493017
akhilraj95 <akhil95raj@gmail.com>
2020-04-23 01:00:15 -0400

    Intermediate commit

--- applications/dsync-editor/src/App.js
+++ applications/dsync-editor/src/App.js
@@ -1,14 +1,74 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import MainPage from './component/MainPage.js'
+import { Button, Grid, Input, Segment, Header, Icon } from 'semantic-ui-react'
+import {createSession} from 'dsynk';
 
-function App() {
-  return (
-    <div className="App">
-      <MainPage/>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  createNewSession() {
+
+    console.log("Creating new seesion");
+
+    createSession("127.0.0.1:9090")
+    .then(sessionInfo => {
+        console.log("Got session ",sessionInfo);
+        this.setState(state => ({
+          'id': sessionInfo.id,
+          'topic': sessionInfo.topic
+        }));
+        console.log("Sesssion created");
+    });
+  }
+
+  render() {
+
+    console.log("render called on app");
+
+    if(this.state.id == undefined) {
+      return (
+        <div className="App">
+          <div className="App-header">
+            <div className="row">
+                <div className="column">
+                  <div className="ui text container inverted segment ">
+                    <Grid verticalAlign='middle' columns={1} centered>
+                      <Grid.Row>
+                        <Header inverted as='h1' icon textAlign='center' color='violet'>
+                          {/* <Icon name='users' circular /> */}
+                          <Header.Content>Dsynk Editor</Header.Content>
+                        </Header>
+                      </Grid.Row>
+                      <Grid.Row>
+                        <Button onClick={() => this.createNewSession()}>Create New Session</Button>
+                      </Grid.Row>
+                      <Grid.Row>
+                        <Segment inverted>
+                          <Input inverted action='Join' placeholder='Session id...' />
+                        </Segment>
+                      </Grid.Row>
+                    </Grid>
+                  </div>
+                </div>
+            </div>          
+          </div>
+        </div>
+      );
+    } else {
+      return (
+
+        <div className="App">
+          <MainPage state={this.state}/>
+        </div>
+      );
+    }
+  }
+
 }
 
 export default App;
--- applications/dsync-editor/src/component/MainPage.js
+++ applications/dsync-editor/src/component/MainPage.js
@@ -2,6 +2,10 @@ import React from 'react';
 import MarkdownIt from 'markdown-it'
 import MdEditor from 'react-markdown-editor-lite'
 import 'react-markdown-editor-lite/lib/index.css';
+import { Menu, Header, Segment, Container } from 'semantic-ui-react'
+import "./MainPage.css"
+import { joinSession, getSession, sendEvent } from 'dsynk';
+
 
 const mdParser = new MarkdownIt(/* Markdown-it options */);
 
@@ -9,29 +13,85 @@ class MainPage extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            text : ""
+        };
+    }
+
+    componentDidMount() {
+        this.joinEditingSession();
+    }
+
+    joinEditingSession() {
+
+        console.log("Joining editing session :" + this.props.state.id);
+
+        if(this.props.state.topic == undefined) {
+            getSession("127.0.0.1:9090", this.props.state.id)
+            .then((sessionInfo) => {this.props.state.topic = sessionInfo.topic});
+        }
+
+        joinSession(this.props.state.topic, (message) => {
+            console.log("message : ", message);
+            this.setState((state) => message);
+        });
     }
 
     handleEditorChange({html, text}) {    
-        console.log('handleEditorChange', html, text)
+        console.log('handleEditorChange', html, text);
+    }
+
+    handleDsynkClick() {
+        console.log('handleDsynkClick');
+    }
+
+    handleEditorChange({html, text}) {
+        // console.log('handleEditorChange', html, text)\
+        sendEvent(this.state.id, text);
     }
 
     render() {
+
         return (
             <div className="mainpage">
-                <MdEditor
-                    value=""
-                    renderHTML={(text) => mdParser.render(text)}
-                    onChange={handleEditorChange}
-                />
+                <Menu>
+                    <Menu.Item header>
+                        <Header as='h1' color='violet'>
+                                {/* <Icon name='users' circular /> */}
+                                <Header.Content>Dsynk Editor</Header.Content>
+                        </Header>
+                    </Menu.Item>
+                    <Menu.Item
+                        name='session-name'>
+                        {this.state.id}
+                    </Menu.Item>
+                </Menu>
+
+                <div className="editor">
+                    <MdEditor
+                        value={this.text}
+                        renderHTML={(text) => mdParser.render(text)}
+                        onChange={(change) => this.handleEditorChange(change)}
+                        config={{
+                            view: {
+                              menu: true,
+                              md: true,
+                              html: true,
+                              fullScreen: true,
+                              hideMenu: true,
+                            },
+                            table: {
+                              maxRow: 6,
+                              maxCol: 6,
+                            },
+                            syncScrollMode: ['leftFollowRight', 'rightFollowLeft'],
+                          }}              
+                    />
+                </div>
             </div>
         );
       };
     
-}
-
-
-function handleEditorChange({html, text}) {    
-    console.log('handleEditorChange', html, text)
 }
 
 
