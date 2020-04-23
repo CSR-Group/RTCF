45fd121a898f5821f5a88f3182f947d51bf09e86
akhilraj95 <akhil95raj@gmail.com>
2020-04-23 04:30:14 -0400

    hub listens to messages

--- dsynk/src/main/java/com/csrg/dsynk/hub/controllers/v1/EventController.java
+++ dsynk/src/main/java/com/csrg/dsynk/hub/controllers/v1/EventController.java
@@ -1,17 +1,34 @@
 package com.csrg.dsynk.hub.controllers.v1;
 
+import com.csrg.dsynk.hub.controllers.v1.models.Event;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+
+import java.util.logging.Logger;
 
 @Controller
 public class EventController {
 
-    @MessageMapping("/session")
-    @SendTo("/topic/messages")
-    public String send(String message) throws Exception {
-        System.out.println("Connected with message" + message);
-        return "{ Hello World: " + message + " }";
+    private static final Logger logger = Logger.getLogger(EventController.class.getName());
+
+    private SimpMessagingTemplate messagingTemplate;
+
+    @Autowired
+    public EventController(SimpMessagingTemplate messagingTemplate) {
+
+        this.messagingTemplate = messagingTemplate;
+    }
+
+
+    @MessageMapping("/trigger")
+    public void triggerEvent(Event event) throws Exception {
+        logger.info("Event received for topic : " + event.getTopic()
+                + " from : " + event.getFrom()
+                + " with message : " + event.getMessage());
+
+        messagingTemplate.convertAndSend(event.getTopic(), event);
     }
 
 }
